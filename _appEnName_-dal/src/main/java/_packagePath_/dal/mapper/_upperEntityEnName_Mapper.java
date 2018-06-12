35ec0a2da856a6539d8daefbd7afccfa1726cb45
shengshan.tang <tss0823@126.com>
2018-06-12 15:14:15 +0800

    fix dal

--- _appEnName_-dal/src/main/java/_packagePath_/dal/mapper/_upperEntityEnName_Mapper.java
+++ _appEnName_-dal/src/main/java/_packagePath_/dal/mapper/_upperEntityEnName_Mapper.java
@@ -2,7 +2,7 @@ package ${packageName}.dal.mapper;
 
 
 
-import ${packageName}.dal.model.${upperEntityEnName};
+import ${packageName}.dal.domain.${upperEntityEnName};
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
